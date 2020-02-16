38ffe1c1e52d0a7ca5c9fff60f76035cb4eceaf9
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2020-02-16 04:01:53 +0100

    Adds color to function printk

--- io.c
+++ io.c
@@ -45,6 +45,33 @@ void printc(char c)
   }
 }
 
+void printc_color(char c, char color)
+/* The 'color' variable indicates the foreground and background color the character is
+ * going to have. To be more specific, the 4 bits with less weight set the foreground
+ * color and the 4 bits with the more weigh set the background color. i.e: the value
+ * 0x41 will display a blue character with a red background. Some values can make the
+ * character blink.
+ */
+{
+  __asm__ __volatile__ ( "movb %0, %%al; outb $0xe9" ::"a"(c)); /* Magic BOCHS debug: writes 'c' to port 0xe9 */
+  if (c=='\n')
+  {
+    x = 0;
+    y=(y+1)%NUM_ROWS;
+  }
+  else
+  {
+    Word ch = (Word) (c & 0x00FF) | (Word) (color << 8); // The color is added just modifying the last part of this line
+    Word *screen = (Word *)0xb8000;
+    screen[(y * NUM_COLUMNS + x)] = ch;
+    if (++x >= NUM_COLUMNS)
+    {
+      x = 0;
+      y=(y+1)%NUM_ROWS;
+    }
+  }
+}
+
 void printc_xy(Byte mx, Byte my, char c)
 {
   Byte cx, cy;
@@ -61,5 +88,5 @@ void printk(char *string)
 {
   int i;
   for (i = 0; string[i]; i++)
-    printc(string[i]);
+    printc_color(string[i], 0x41);
 }
