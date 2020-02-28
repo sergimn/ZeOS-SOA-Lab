ff3f83e3ac49419594fa45d7e24d91f6d99c8e6e
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2020-02-28 01:37:27 +0100

    Adds write Service Routine

--- sys.c
+++ sys.c
@@ -45,3 +45,32 @@ int sys_fork()
 void sys_exit()
 {  
 }
+
+#define K_BUFFER_SIZE 256
+char kern_buffer [K_BUFFER_SIZE];
+
+int sys_write(int fd, char * buffer, int size)
+//fd: file descriptor. In this delivery it must always be 1.
+//buffer: pointer to the bytes.
+//size: number of bytes.
+//return Negative number in case of error (specifying the kind of error) and the number of bytes written if OK.
+{
+  int is_fd_error=check_fd(fd, ESCRIPTURA);
+  if (is_fd_error) return is_fd_error;
+  if (buffer==NULL) return -14;  //This value is EFAULT. Will be replaced later
+  if (size<0) return -22;        //This value is EINVAL. Will be replaced later
+
+  int bytes_left=size;
+  int ret;
+  while (bytes_left>K_BUFFER_SIZE)
+  {
+    copy_from_user(buffer+(size-bytes_left), kern_buffer, K_BUFFER_SIZE);
+    ret=sys_write_console(kern_buffer, K_BUFFER_SIZE);
+    bytes_left-=ret;
+  }
+  copy_from_user(buffer+(size-bytes_left), kern_buffer, bytes_left);
+  ret=sys_write_console(kern_buffer, bytes_left);
+  bytes_left-=ret;
+  return size-bytes_left;
+
+}
--- user.c
+++ user.c
@@ -11,5 +11,6 @@ int __attribute__ ((__section__(".text.main")))
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
     
+  write(1, "\nWe are now writting from userspace\n", 36);
   while(1) { }
 }
